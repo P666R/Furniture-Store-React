4313ad83044ac17c2b4f9dd62cae766914322d49
PRoy <sam469roger@gmail.com>
2023-04-07 20:51:20 +0530

    setup productpage and helpers

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,12 +1,29 @@
-import React from 'react'
-import styled from 'styled-components'
-import { formatPrice } from '../utils/helpers'
-import { FaSearch } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import React from "react";
+import styled from "styled-components";
+import { formatPrice } from "../utils/helpers";
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
-const Product = () => {
-  return <h4>product</h4>
-}
+/* 
+  <img src={`https://unblockweb.one/?cdURL=${image}`} alt={name} />; 
+  */
+
+const Product = ({ image, name, price, id }) => {
+  return (
+    <Wrapper>
+      <div className="container">
+        <img src={image} alt={name} />
+        <Link to={`/products/${id}`} className="link">
+          <FaSearch />
+        </Link>
+      </div>
+      <footer>
+        <h5>{name}</h5>
+        <p>{formatPrice(price)}</p>
+      </footer>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.article`
   .container {
@@ -63,5 +80,5 @@ const Wrapper = styled.article`
     color: var(--clr-primary-5);
     letter-spacing: var(--spacing);
   }
-`
-export default Product
+`;
+export default Product;
